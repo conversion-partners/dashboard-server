9cabb54252851dc83746bbdd438d0c651d627d84
Robert Cooke <robert.cooke@trimm.nl>
2015-08-29 11:51:36 +0200

    cc

--- index.php
+++ index.php
@@ -1,49 +1,11 @@
 <?php
 
-$domain = array('easydrain.com' => 'easydrain', 'easydrain.nl' => 'easydrain');
-$origin = array('http://localhost:9090/' => 'active', 'http://localhost:9090/' => 'active');
+require 'Singleton.php';
+include_once dirname(__FILE__).'/config.php';
 
-class Cache
-{
+if ($config->getDebug()) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
 }
 
-class Request
-{
-    private $requestObject;
-}
-
-class Init
-{
-    private $conf;
-    public function start()
-    {
-        echo $this->var;
-    }
-}
-
-// THREE STEPS TO USE LIGHTNCANDY
-// Step 1. require the lib, compile template, and get the PHP code as string
-require 'vendor/zordius/lightncandy/src/lightncandy.php';
-
-$template = "Welcome {{name}} , You win \${{value}} dollars!!\n";
-$phpStr = LightnCandy::compile($template);  // compiled PHP code in $phpStr
-
-// Step 2A. (Usage 1) use LightnCandy::prepare to get rendering function
-//   DEPRECATED , it may require PHP setting allow_url_fopen=1 ,
-//   and allow_url_fopen=1 is not secure .
-//   When allow_url_fopen = 0, prepare() will create tmp file then include it,
-//   you will need to add your tmp directory into open_basedir.
-//   YOU MAY NEED TO CHANGE PHP SETTING BY THIS WAY
-$renderer = LightnCandy::prepare($phpStr);
-
-// Step 2B. (Usage 2) Store your render function in a file
-//   You decide your compiled template file path and name, save it.
-//   You can load your render function by include() later.
-//   RECOMMENDED WAY
-file_put_contents($php_inc, $phpStr);
-$renderer = include $php_inc;
-
-// Step 3. run native PHP render function any time
-echo "Template is:\n$template\n\n";
-echo $renderer(array('name' => 'John', 'value' => 10000));
-echo $renderer(array('name' => 'Peter', 'value' => 1000));
+include_once dirname(__FILE__).'/app.php';
