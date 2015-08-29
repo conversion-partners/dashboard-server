bfcac8ecc232d292e73d576ef5adff37f439e1dd
Robert Cooke <robert.cooke@trimm.nl>
2015-08-29 21:19:11 +0200

    cc

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'phpDocumentor' => array($vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Monolog' => array($vendorDir . '/monolog/monolog/src'),
     'Dashboardserver' => array($vendorDir . '/conversion-partners/dashboard-server-package/src'),
 );
